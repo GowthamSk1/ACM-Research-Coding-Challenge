cf9e179074635c01aeef3dd0a6e04f55ecb1699d
GowthamSk1 <gxs180007@utdallas.edu>
2020-09-09 20:49:42 -0500

    edit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'''
+1. Use Built-in K-means Clustering Algorithm (Sklearn)
+2. Determine the number of clusters in a dataset
+3. Calculate Optimal K value (Optional Clusters)
+4. Plot Elbow Method and ClusterPlot.csv with clusters
+
+'''
 import numpy as np
 import pandas as pd  
 from sklearn.cluster import KMeans
@@ -22,7 +29,9 @@ for i in range(1, 7):  # for K clusters calculate the distortion
     model = KMeans(n_clusters=i).fit(X)  
     model.fit(X) 
     dis.append(sum(np.min(cdist(X, model.cluster_centers_, 'euclidean'), axis = 1)) / X.shape[0])
-    
+kmeans = KMeans(n_clusters=3).fit(csv_data)
+cent = kmeans.cluster_centers_
+   
 ''' PLOT OPTIMAL K '''
 plt.plot(range(1, 7), dis, 'bx-')
 plt.xlabel('k (Number of Clusters)')
@@ -31,8 +40,9 @@ plt.title('Elbow Method (CLUSTERING) to find the optimal k')
 plt.show()
 
 ''' PLOT CENTROIDS USING OPTIMAL CLUSTERS '''
-kmeans = KMeans(n_clusters=3).fit(csv_data)
-cent = kmeans.cluster_centers_
-plt.scatter(csv_data['V1'], csv_data['V2'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+plt.scatter(csv_data['V1'], csv_data['V2'], c= kmeans.labels_.astype(float), s=20, alpha=0.5)
 plt.scatter(cent[:, 0], cent[:, 1], c='red', s=50)
 plt.show()
+
+
+# Sources cited in the README
